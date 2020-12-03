185e875f704516c712204c1d7e4c02b5472543b2
DonOllario <oliver.stenstrom@hotmail.com>
2020-12-03 19:59:30 +0100

    First Commit

---
+++ Projektarbete-E-Commerce-KOTR/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Projektarbete_E_Commerce_KOTR
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello Kevin, Oliver, Tobias, Rikard. This is the start of our E-Commerce");
+        }
+    }
+}
