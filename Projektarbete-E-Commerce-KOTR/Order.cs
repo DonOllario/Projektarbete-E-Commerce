f09fc84c43da9ebda9138639df399113585d63b5
NyholmTobias <nyholm.tobias@gmail.com>
2020-12-08 10:32:06 +0100

    Commit 2 för merch

--- Projektarbete-E-Commerce-KOTR/Order.cs
+++ Projektarbete-E-Commerce-KOTR/Order.cs
@@ -11,30 +11,54 @@ namespace Projektarbete_E_Commerce_KOTR
         public double TotalPrice;
         public int OrderID;
         public string DeliveryAdress;
+        /*public List<ProductLine> OrderLines;*/
+        public bool Invoice = false;
+        public bool CreditCard = false;
+        public string NameOnCard;
+        public int CreditCardNumber;
+        public int CVCcode;
+        public string NameOnInvoice;
+        public string InvoiceAdress;
 
-        public Order(ProductLine[] productLines, string userAdress)
+        public Order(/*List<ProductLine> productLines,*/ string userAdress, string userName, int shoppingCartID)
         {
-            OrderID = OrderID++;
-            CalcTotalPrice(productLines);
+            OrderID = shoppingCartID;
+            /*OrderLines = productLines;*/
+            /*CalcTotalPrice(productLines);*/
             if (userAdress != null)
             {
-                DeliveryAdress = userAdress; ;
+                DeliveryAdress = userAdress;
             }
             else
             {
                 GetAdressFromUserThroughInput();
             }
+            int payment = GetPaymentChoiseFromUser();
+            if (payment == 1)
+            {
+                NameOnCard = GetNameOnCard();
+                CreditCardNumber = GetCreditCardNumber();
+                CVCcode = GetCVCcode();
+                CheckAccountBalance(TotalPrice);
+                CreditCard = true;
+            }
+            else if(payment == 2)
+            {
+                NameOnInvoice = GetNameOnInvoice(userName);
+                InvoiceAdress = GetInvoiceAdress(userAdress);
+                Invoice = true;
+            }
         }
 
-        public void CalcTotalPrice(ProductLine[] productLine)
+        /*public void CalcTotalPrice(List<ProductLine> productLines)
         {
             double totalPrice = 0;
-            for (int i = 0; i < productLine.Length; i++)
+            foreach (ProductLine p in productLines)
             {
-                totalPrice += productLine[i].Price;
+                totalPrice += productLines[p].Price;
             }
             TotalPrice = totalPrice;
-        }
+        }*/
 
         public void GetAdressFromUserThroughInput()
         {
@@ -42,6 +66,105 @@ namespace Projektarbete_E_Commerce_KOTR
             Console.Write("Vänligen ange leveransadressen: ");
             adress = Console.ReadLine();
             DeliveryAdress = adress;
+        }
+
+        public int GetPaymentChoiseFromUser()
+        {
+            Console.Clear();
+            Console.WriteLine("Choose your payment method.{0}1. Credit card.{0}2. Invoice.", Environment.NewLine);
+            int paymentChoise = 0;
+            while (paymentChoise != 1 && paymentChoise != 2)
+            {
+                paymentChoise = int.Parse(Console.ReadLine());
+                switch (paymentChoise)
+                {
+                    case 1:
+                        break;
+                    case 2:
+                        break;
+                    default:
+                        Console.Clear();
+                        Console.WriteLine("You need to enter 1 or 2.");
+                        break;
+                }
+            }
+            return paymentChoise;
+        }
+        public string GetNameOnCard()
+        {
+            Console.WriteLine("Please enter the following information.");
+            Console.Write("Name on card: ");
+            string nameOnCard = Console.ReadLine();
+            return nameOnCard;
+        }
+        public int GetCreditCardNumber()
+        {
+            int creditCardNumber = 0;
+            while (Convert.ToString(creditCardNumber).Length != 16)
+            {
+                Console.Write("Credit card number: ");
+                CreditCardNumber = int.Parse(Console.ReadLine());
+                if (Convert.ToString(creditCardNumber).Length != 16)
+                {
+                    Console.Clear();
+                    Console.WriteLine("You need to enter the number with 16 digits and no spaces. Press enter once and try again.");
+                }
+            }
+            return creditCardNumber;
+        }
+        public int GetCVCcode()
+        {
+            int cvcCode = 0;
+            while (Convert.ToString(cvcCode).Length != 3)
+            {
+                Console.Write("CVC-code: ");
+                CVCcode = int.Parse(Console.ReadLine());
+                if (Convert.ToString(cvcCode).Length != 3)
+                {
+                    Console.Clear();
+                    Console.WriteLine("The CVC-code is 3 digits. Press enter once and try again.");
+                }
+            }
+            return cvcCode;
+        }
+        public string GetNameOnInvoice(string userName)
+        {
+            string invoiceName;
+            if(userName != null)
+            {
+                Console.Write("Enter the name that should be put on the invoice: ");
+                invoiceName = Console.ReadLine();
+            }
+            else
+            {
+                invoiceName = userName;
+            }
+            return invoiceName;
+        }
+        public string GetInvoiceAdress(string userAdress)
+        {
+            string invoiceAdress;
+            if (userAdress != null)
+            {
+                Console.Write("Enter the adress that we should sent the invoice too: ");
+                invoiceAdress = Console.ReadLine();
+            }
+            else
+            {
+                invoiceAdress = userAdress;
+            }
+            return invoiceAdress;
+        }
+        public bool CheckAccountBalance(double totalPrice)
+        {
+            bool balance = false;
+            Bank bank1 = new Bank();
+            balance = bank1.CheckPayment(totalPrice);
+            if (balance == false)
+            {
+                Console.WriteLine("You dont have enough money on you bank account. Please try another payment method.");
+            }
+            return balance;
         }
     }
 }
--- Projektarbete-E-Commerce-KOTR/Program.cs
+++ Projektarbete-E-Commerce-KOTR/Program.cs
@@ -8,19 +8,9 @@ namespace Projektarbete_E_Commerce_KOTR
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
-            ProductLine test = new ProductLine(5);
-            ProductLine test1 = new ProductLine(5);
-            ProductLine test2 = new ProductLine(5);
-            ProductLine test3 = new ProductLine(5);
-            ProductLine test4 = new ProductLine(5);
 
-            ProductLine[] productLines = { test, test1, test2, test3, test4 };
-
-            Order testOrder = new Order(productLines, null);
-
-            Console.WriteLine($"Totalpriset är: {testOrder.TotalPrice}:- och leveransadressen är: {testOrder.DeliveryAdress}.");
         }
     }
 }
